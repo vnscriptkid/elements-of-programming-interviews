48e216af69299231cbbcb5bbb00ebca76c950fec
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-04 09:29:50 +0700

    feat(linkedlist): bootcamp submit 1

---
+++ 4-linkedlists/0-bootcamp/submit-1.ts
@@ -0,0 +1,41 @@
+class Node<T> {
+  val: T;
+  next: Node<T>;
+
+  constructor(val: T, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+
+  /**
+   * @param  {Node<number>} head
+   * @param  {number} key
+   * @returns Node
+   */
+  static search(head: Node<number>, key: number): Node<number> | null {
+    let cur = head;
+
+    while (cur) {
+      if (cur.val === key) return cur;
+
+      cur = cur.next;
+    }
+
+    return null;
+  }
+
+  static insertAfter(node: Node<number>, newNode: Node<number>): void {
+    const oldNext = node.next;
+
+    node.next = newNode;
+
+    newNode.next = oldNext;
+  }
+
+  // delete a node immedidately following a node. Assumes aNode is not a tail
+  static deleteRightAfter(aNode: Node<number>) {
+    aNode.next = aNode.next.next;
+  }
+}
+
+export { Node };
---
+++ 4-linkedlists/0-bootcamp/test.ts
@@ -0,0 +1,31 @@
+import { Node } from "./submit-1";
+
+test("search", () => {
+  const node1 = new Node(1);
+  const node2 = new Node(2);
+  const node3 = new Node(3);
+
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = null;
+
+  expect(Node.search(node1, 3)).toEqual(node3);
+  expect(Node.search(node1, 2)).toEqual(node2);
+  expect(Node.search(node1, 4)).toEqual(null);
+});
+
+test("insertAfter", () => {
+  const node1 = new Node(1);
+  const node2 = new Node(2);
+  const node3 = new Node(3);
+
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = null;
+
+  const node4 = new Node(4);
+  Node.insertAfter(node2, node4);
+
+  expect(node2.next).toEqual(node4);
+  expect(node4.next).toEqual(node3);
+});
