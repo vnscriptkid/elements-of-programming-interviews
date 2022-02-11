2eb118605942d48f686b073dfcbde7b014529681
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-11 11:39:16 +0700

    chore: no need eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: ["standard", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  // env: {
+  //   browser: true,
+  //   es2021: true,
+  //   jest: true,
+  // },
+  // extends: ["standard", "prettier"],
+  // parser: "@typescript-eslint/parser",
+  // parserOptions: {
+  //   ecmaVersion: "latest",
+  //   sourceType: "module",
+  // },
+  // plugins: ["@typescript-eslint"],
+  // rules: {},
 };
