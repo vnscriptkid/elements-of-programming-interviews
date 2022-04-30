0de62ae3a6f18611f084a3338aa7d8e27a4ecbcc
Thanh Nguyen <vnscriptkid@gmail.com>
2022-04-30 10:43:22 +0700

    chore: rename folder

--- 5-stack-and-queue/9.6/submit-1.test.ts
+++ 5-stack-and-queue/9.6/submit-1.test.ts
@@ -2,6 +2,5 @@ import { examineBuildingsWithSunset } from "./submit-1";
 
 test("it works 1", () => {
   const result = examineBuildingsWithSunset([5, 3, 6, 4, 5, 6, 8]);
-  console.log({ result });
   expect(result).toEqual([0, 2, 6]);
 });
