19f08e648a0a700babc5746554cc4d83cc75e6e7
Thanh Nguyen <vnscriptkid@gmail.com>
2022-06-17 22:57:05 +0700

    feat: FIND THE LENGTH OF A LONGEST CONTAINED INTERVAL submit 1

---
+++ 9-hashtable/13.10-length-longest-contained-interval/starter.ts
@@ -0,0 +1,13 @@
+// 13.10 FIND THE LENGTH OF A LONGEST CONTAINED INTERVAL
+
+// Write a program which takes asinput a set of integers represented by an array, and
+// returns the size of a largest subset of integers in the array having the property that if
+// two integers are in the subset, then so are all integers between them. For example, if
+// the input is(3,-2,7,9,8,1,2,0,-1,5,8), the largest such subset is{-2,-1,0,1,2,3),so
+// you should return 6.
+
+// Hint: Do you really need a total ordering on the input?
+
+export function longestContainedRange(list: number[]): number {
+    return 0;
+}
---
+++ 9-hashtable/13.10-length-longest-contained-interval/submit-1.test.ts
@@ -0,0 +1,5 @@
+import { longestContainedRange } from "./submit-1"
+
+test('it works', () => {
+    expect(longestContainedRange([3,-2,7,9,8,1,2,0,-1,5,8])).toEqual(6)
+})
