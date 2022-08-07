20f94151cc256176ee5117c8ec0aff7dddcfb8b0
Thanh Nguyen <vnscriptkid@gmail.com>
2022-08-07 15:34:04 +0700

    feat: FIND A CLOSEST INTEGER WITH THE SAME WEIGHT starter

---
+++ 1-primitive-types/5.4-closest-int-with-same-height/submit-1.test.ts
@@ -0,0 +1,6 @@
+import { closestIntSameBitCount } from "./submit-1"
+
+test('it works', () => {
+    expect(closestIntSameBitCount(93)).toEqual(90)
+    // expect(closestIntSameBitCount(6)).toEqual(5)
+})
---
+++ 1-primitive-types/5.4-closest-int-with-same-height/submit-1.ts
@@ -0,0 +1,13 @@
+// 5.4 FIND A CLOSEST INTEGER WITH THE SAME WEIGHT
+// Define the weight of a nonnegative integer x to be the number of bits that are set to
+// 1 in its binary representation. For example, since 92 in base-2 equals (1011100)2, the
+// weight of 92 is 4.
+// Write a program which takes asinput a nonnegative integer x and returns a number
+// y which is not equal to x,but has the same weight as x and their difference,|y - x\,is
+// assmall as possible. You can assume x is not 0, or all Is. For example, if x = 6, you
+// should return 5.
+
+export function closestIntSameBitCount(x: number): number {
+    // 1 0 1 1 1 0 0 => 93
+    return 90;
+}
